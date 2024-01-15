0c94c6bed374308db272fb312ebc6ffd9ce8b249
Iryna Lytvyn <i.litvin27@gmail.com>
2024-01-15 18:14:39 +0200

    optimised code

--- spotify_data_manipulation/spotify_client.py
+++ spotify_data_manipulation/spotify_client.py
@@ -1,16 +1,11 @@
 """Client for spotify API."""
 import base64
-from typing import Union
+from typing import Callable, Union
 
 import requests
 from requests.models import Response
 
-GOOD_STATUS_CODE: int = 200
 DEFAULT_TIMEOUT: int = 30
-
-
-class NoAccessTokenError(Exception):
-    """Access token is not received exception."""
 
 
 class SpotifyClient(object):
@@ -26,9 +21,40 @@ class SpotifyClient(object):
         :param client_secret: client secret provided by spotify
         """
         self.access_token: str = self.retrieve_access_token(client_id, client_secret)
+        self.tracks: Track = Track(self)
+        self.albums: Album = Album(self)
+        self.artists: Artist = Artist(self)
 
     @classmethod
-    def retrieve_access_token(cls, client_id: str, client_secret: str) -> str:
+    def send_request(
+        cls,
+        method: str,
+        timeout: int = DEFAULT_TIMEOUT,
+        *args,
+        **kwargs,
+    ) -> dict:
+        """
+        Send requests. Base method.
+
+        :param method: HTTP method
+        :param timeout: request timeout
+        :param args: request args
+        :param kwargs: request kwargs
+        :return: response json
+        :raises: HTTPError if request failed
+        """
+        method_func: Callable = getattr(requests, method.lower(), None)
+        if method_func is None:
+            raise ValueError('method {0} does not exist'.format(method))
+        response: Response = method_func(
+            *args,
+            timeout=timeout,
+            **kwargs,
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def retrieve_access_token(self, client_id: str, client_secret: str) -> str:
         """
         Get access token from spotify.
 
@@ -38,15 +64,13 @@ class SpotifyClient(object):
         :raises: Exception if can not get access token
         """
         encoded: bytes = base64.b64encode('{0}:{1}'.format(client_id, client_secret).encode())
-        response: Response = requests.post(
-            'https://accounts.spotify.com/api/token',
-            data={'grant_type': 'client_credentials'},
+        response = self.send_request(
+            method='post',
+            url='https://accounts.spotify.com/api/token',
             headers={'Authorization': 'Basic {0}'.format(encoded.decode())},
-            timeout=DEFAULT_TIMEOUT,
+            data={'grant_type': 'client_credentials'},
         )
-        if response.status_code == GOOD_STATUS_CODE:
-            return response.json()['access_token']
-        raise NoAccessTokenError('Can not get access token')
+        return response['access_token']
 
     def get_item(self, endpoint: str) -> Union[dict, str]:
         """
@@ -55,15 +79,23 @@ class SpotifyClient(object):
         :param endpoint: spotify API path to get item's information. Example: tracks/11dFghVXANMlKmJXsNCbNl
         :return: response json from spotify. If request failed return string with response.status_code
         """
-        response: Response = requests.get(
-            '{0}/{1}'.format(self.spotify_url, endpoint),
+        return self.send_request(
+            method='get',
+            url='{0}/{1}'.format(self.spotify_url, endpoint),
             headers={'Authorization': 'Bearer {0}'.format(self.access_token)},
-            timeout=DEFAULT_TIMEOUT,
         )
 
-        if response.status_code == GOOD_STATUS_CODE:
-            return response.json()
-        return 'Error {0}'.format(response.status_code)
+
+class Track(object):
+    """Class for spotify API track data."""
+
+    def __init__(self, spotify_client: SpotifyClient) -> None:
+        """
+        Initialise spotify_client.
+
+        :param spotify_client: SpotifyClient instance
+        """
+        self.spotify_client: SpotifyClient = spotify_client
 
     def get_track(self, track_id: str) -> Union[dict, str]:
         """
@@ -72,7 +104,19 @@ class SpotifyClient(object):
         :param track_id: track id in spotify
         :return: dict with track data
         """
-        return self.get_item('tracks/{0}'.format(track_id))
+        return self.spotify_client.get_item('tracks/{0}'.format(track_id))
+
+
+class Artist(object):
+    """Class for spotify API artist data."""
+
+    def __init__(self, spotify_client: SpotifyClient) -> None:
+        """
+        Initialise spotify_client.
+
+        :param spotify_client: SpotifyClient instance
+        """
+        self.spotify_client: SpotifyClient = spotify_client
 
     def get_artist(self, artist_id: str) -> Union[dict, str]:
         """
@@ -81,7 +125,19 @@ class SpotifyClient(object):
         :param artist_id: artist id in spotify
         :return: dict with artist data
         """
-        return self.get_item('artists/{0}'.format(artist_id))
+        return self.spotify_client.get_item('artists/{0}'.format(artist_id))
+
+
+class Album(object):
+    """Class for spotify API album data."""
+
+    def __init__(self, spotify_client: SpotifyClient) -> None:
+        """
+        Initialise spotify_client.
+
+        :param spotify_client: SpotifyClient instance
+        """
+        self.spotify_client: SpotifyClient = spotify_client
 
     def get_album(self, album_id: str) -> Union[dict, str]:
         """
@@ -90,4 +146,4 @@ class SpotifyClient(object):
         :param album_id: album id in spotify
         :return: dict with album data
         """
-        return self.get_item('albums/{0}'.format(album_id))
+        return self.spotify_client.get_item('albums/{0}'.format(album_id))
